97269a4e6a111c2e5cde08bb2f4d1200bd3652b3
jose antonio benito <jose antonio benito>
2016-02-03 11:29:42 +0100

    Nuevo

---
+++ Agenda-v.1/src/benito/agenda/Main.java
@@ -0,0 +1,10 @@
+package benito.agenda;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Agenda-v.1/src/benito/test/TestePruebas.java
@@ -0,0 +1,14 @@
+package benito.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestePruebas {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
